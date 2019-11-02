d03e5b48a4e8dd7673e41d8cde3d8fd649a2c9d8
jneilliii <jneilliii+github@gmail.com>
2019-11-01 23:23:54 -0400

    0.5.5
    - Added fontawesome icon picker to make the class naming easier on the end user.

--- octoprint_taborder/__init__.py
+++ octoprint_taborder/__init__.py
@@ -9,8 +9,8 @@ class taborder(octoprint.plugin.AssetPlugin,
 	
 	##-- AssetPlugin mixin
 	def get_assets(self):
-		return dict(js=["js/taborder.js","js/spectrum.js"],
-					css=["css/taborder.css","css/font-awesome.min.css","css/spectrum.css","css/font-awesome-v4-shims.min.css"])
+		return dict(js=["js/taborder.js","js/spectrum.js","js/fontawesome-iconpicker.js","js/ko.iconpicker.js"],
+					css=["css/taborder.css","css/font-awesome.min.css","css/spectrum.css","css/font-awesome-v4-shims.min.css","css/fontawesome-iconpicker.css"])
 		
 	##-- Settings mixin
 	def get_settings_defaults(self):
--- octoprint_taborder/static/js/taborder.js
+++ octoprint_taborder/static/js/taborder.js
@@ -56,9 +56,9 @@ $(function() {
 					$('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').text('');
 				}
 				if ($('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').children('i').length > 0) {
-					$('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').attr('title',tab.icon_tooltip()).children('i').addClass(tab.icon()).css({'color':tab.icon_color()});
+					$('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').attr('title',tab.icon_tooltip()).children('i').addClass(tab.icon() + ' fa-lg').css({'color':tab.icon_color()});
 				} else {
-					$('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').attr('title',tab.icon_tooltip()).prepend('<i class="'+tab.icon()+'" style="color:'+tab.icon_color()+'"></i> ');
+					$('li#'+tabid+'_link a,li#tab_'+tabid+'_link a').attr('title',tab.icon_tooltip()).prepend('<i class="'+tab.icon()+' fa-lg" style="color:'+tab.icon_color()+'"></i> ');
 				}
 			});
 		}
