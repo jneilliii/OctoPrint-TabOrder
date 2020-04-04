66d7b8cf9a59b3e9e2f0f10f837982eb6eff6417
jneilliii <jneilliii+github@gmail.com>
2020-04-04 14:50:46 -0400

    fix memory leak

--- octoprint_taborder/static/js/bootstrap-tabdrop.js
+++ octoprint_taborder/static/js/bootstrap-tabdrop.js
@@ -1,10 +1,8 @@
 /* =========================================================
- * bootstrap-tabdrop.js
+ * bootstrap-tabdrop.js 
  * http://www.eyecon.ro/bootstrap-tabdrop
  * =========================================================
  * Copyright 2012 Stefan Petre
- * Copyright 2013 Jenna Schabdach
- * Copyright 2014 Jose Ant. Aranda
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,154 +16,93 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================= */
+ 
+!function( $ ) {
 
-!function ($) {
-
-	var WinResizer = (function () {
+	var WinReszier = (function(){
 		var registered = [];
 		var inited = false;
 		var timer;
-		var resize = function () {
+		var resize = function(ev) {
 			clearTimeout(timer);
 			timer = setTimeout(notify, 100);
 		};
-		var notify = function () {
-			for (var i = 0, cnt = registered.length; i < cnt; i++) {
+		var notify = function() {
+			for(var i=0, cnt=registered.length; i<cnt; i++) {
 				registered[i].apply();
 			}
 		};
 		return {
-			register: function (fn) {
+			register: function(fn) {
 				registered.push(fn);
 				if (inited === false) {
-					$(window).on('resize', resize);
+					$(window).bind('resize', resize);
 					inited = true;
 				}
 			},
-			unregister: function (fn) {
-				var registeredFnIndex = registered.indexOf(fn);
-				if (registeredFnIndex > -1) {
-					registered.splice(registeredFnIndex, 1);
+			unregister: function(fn) {
+				for(var i=0, cnt=registered.length; i<cnt; i++) {
+					if (registered[i] == fn) {
+						delete registered[i];
+						break;
+					}
 				}
 			}
 		}
 	}());
 
-	var TabDrop = function (element, options) {
+	var TabDrop = function(element, options) {
 		this.element = $(element);
-		this.options = options;
-
-		if (options.align === "left")
-			this.dropdown = $('<li class="dropdown hide pull-left tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="javascript:;"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
-		else
-			this.dropdown = $('<li class="dropdown hide pull-right tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="javascript:;"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
-
-		this.dropdown.prependTo(this.element);
+		this.hidden = options.hidden;
+		this.dropdown = $('<li class="dropdown hide pull-right tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="#">'+options.text+' <b class="caret"></b></a><ul class="dropdown-menu"></ul></li>')
+							.prependTo(this.element);
 		if (this.element.parent().is('.tabs-below')) {
 			this.dropdown.addClass('dropup');
 		}
-
-		var boundLayout = $.proxy(this.layout, this);
-
-		WinResizer.register(boundLayout);
-		this.element.on('shown.bs.tab', function (e) {
-   		boundLayout();
- 		});
-
-		this.teardown = function () {
-			WinResizer.unregister(boundLayout);
-			this.element.off('shown.bs.tab', function (e) {
-				boundLayout();
-			});
-		};
-
+		WinReszier.register($.proxy(this.layout, this));
 		this.layout();
 	};
 
 	TabDrop.prototype = {
 		constructor: TabDrop,
 
-		layout: function () {
-			var self = this;
+		layout: function() {
 			var collection = [];
-			var isUsingFlexbox = function(el){
-				return el.element.css('display').indexOf('flex') > -1;
-			};
-
-			function setDropdownText(text) {
-				self.dropdown.find('a span.display-tab').html(text);
-			}
-
-			function setDropdownDefaultText(collection) {
-				var text;
-				if (jQuery.isFunction(self.options.text)) {
-					text = self.options.text(collection);
+			var hidden = this.hidden;
+			this.dropdown.removeClass('hide');
+			this.element
+				.append(this.dropdown.find('li'))
+				.find('>li')
+				.not('.tabdrop')
+				.each(function(){
+					if(this.offsetTop > 0 || hidden.indexOf(this.id) > -1) {
+						collection.push(this);
+					}
+				});
+			if (collection.length > 0) {
+				collection = $(collection);
+				this.dropdown
+					.find('ul')
+					.empty()
+					.append(collection);
+				if (this.dropdown.find('.active').length == 1) {
+					this.dropdown.addClass('active');
 				} else {
-					text = self.options.text;
+					this.dropdown.removeClass('active');
 				}
-				setDropdownText(text);
+			} else {
+				this.dropdown.addClass('hide');
 			}
-
-			// Flexbox support
-			function handleFlexbox(){
-				if (isUsingFlexbox(self)){
-					if (self.element.find('li.tabdrop').hasClass('pull-right')){
-						self.element.find('li.tabdrop').css({position: 'absolute', right: 0});
-						self.element.css('padding-right', self.element.find('.tabdrop').outerWidth(true));
-					}
-				}
-			}
-
-			function checkOffsetAndPush(recursion) {
-				self.element.find('> li:not(.tabdrop)')
-					.each(function () {
-						if (this.offsetTop > self.options.offsetTop) {
-							collection.push(this);
-						}
-						if(self.options.hidden.indexOf(this.id) > -1){
-							collection.push(this);
-						}
-					});
-
-				if (collection.length > 0) {
-					if (!recursion) {
-						self.dropdown.removeClass('hide');
-						self.dropdown.find('ul').empty();
-					}
-					self.dropdown.find('ul').prepend(collection);
-					
-					if (self.dropdown.find('.active').length == 1) {
-						self.dropdown.addClass('active');
-						setDropdownText(self.dropdown.find('.active > a').html());
-					} else {
-						self.dropdown.removeClass('active');
-						setDropdownDefaultText(collection);
-					}
-					handleFlexbox();
-					collection = [];
-					checkOffsetAndPush(true);
-				} else {
-					if (!recursion) {
-						self.dropdown.addClass('hide');
-					}
-				}
-			}
-
-			self.element.append(self.dropdown.find('li'));
-			checkOffsetAndPush();
-			self.element.trigger('tabdrop.layout.complete');
 		}
-	};
+	}
 
-	$.fn.tabdrop = function (option) {
+	$.fn.tabdrop = function ( option ) {
 		return this.each(function () {
 			var $this = $(this),
 				data = $this.data('tabdrop'),
 				options = typeof option === 'object' && option;
-			if (!data) {
-				options = $.extend({}, $.fn.tabdrop.defaults, options);
-				data = new TabDrop(this, options);
-				$this.data('tabdrop', data);
+			if (!data)  {
+				$this.data('tabdrop', (data = new TabDrop(this, $.extend({}, $.fn.tabdrop.defaults,options))));
 			}
 			if (typeof option == 'string') {
 				data[option]();
@@ -175,10 +112,9 @@
 
 	$.fn.tabdrop.defaults = {
 		text: '<i class="icon-align-justify"></i>',
-		offsetTop: 0,
 		hidden: []
 	};
 
 	$.fn.tabdrop.Constructor = TabDrop;
 
-}(window.jQuery);
+}( window.jQuery );
